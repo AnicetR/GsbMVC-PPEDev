35164526841b1d3cc6ff3117a7a3dd1941728b79
Anicet REGLAT VIZZAVONA <anicet.reglat@gmail.com>
2016-04-11 01:13:33 +0200

    Revert "Revert "Modifications API""
    This reverts commit 28a6775c34101dd69de4e23005f46c5b492fdd39.

--- app/controllers/API/MainAPI.php
+++ app/controllers/API/MainAPI.php
@@ -4,6 +4,7 @@ namespace App\Controllers\API;
 use App\Models\User;
 use Base;
 use App\Models\ApiAccessManager;
+use App\Helpers\SMS;
 
 class MainAPI{
 
@@ -32,20 +33,56 @@ class MainAPI{
         $userId = null;
 
 
-        if ($this->f3->get('SERVER.PHP_AUTH_USER') !== null && $this->f3->get('SERVER.PHP_AUTH_PW') !== null ) {
+        if ($this->f3->get('SERVER.PHP_AUTH_USER') !== null && $this->f3->get('SERVER.PHP_AUTH_PW') !== null && $this->f3->get('SERVER.PHP_AUTH_PW') !== '') {
             $phoneNumber = $this->f3->get('SERVER.PHP_AUTH_USER');
             $apiKey = $this->f3->get('SERVER.PHP_AUTH_PW');
             $userId = $this->accessManager->getAccess($phoneNumber, $apiKey);
         }
+
+        else if ($this->f3->get('SERVER.PHP_AUTH_USER') !== null && $this->f3->get('SERVER.PHP_AUTH_PW') == '' && $this->f3->get('GET') !== null) {
+            $phoneNumber = $this->f3->get('SERVER.PHP_AUTH_USER');
+            $getDatas = $this->f3->get('GET');
+            if ($APIkey = $this->Authorize($phoneNumber, $getDatas)) {
+                echo json_encode($APIkey);
+                die();
+            }
+        }
+
         if (is_null($userId)){
             header('WWW-Authenticate: Basic realm="GSB"');
             header('HTTP/1.0 401 Unauthorized');
             echo json_encode(false);
             die();
         }
+
         else {
             $this->f3->set('userId', $userId);
         }
+    }
+
+    public function Authorize($phoneNumber, $getData){
+        $APIaccess = new ApiAccessManager();
+        $expected = ['uuid', 'model', 'platform'];
+        $tempData = [];
+        foreach ($getData as $key => $value) {
+            if(in_array($key, $expected)){
+
+                $tempData[$key] = $value;
+            }
+        }
+        if(count($tempData) == count($expected)){
+            if($APIaccess->phoneExists($phoneNumber)){
+                if($infos = $APIaccess->addPhone($phoneNumber, $tempData)){
+                    SMS::send($phoneNumber, $infos['Token']);
+                    return $infos['ApiKey'];
+                }
+            }
+        }
+    }
+
+    protected function getPUT(){
+        $body = $this->f3->get('BODY');
+        return json_decode($body, true);
     }
 
 }
--- app/models/ApiAccessManager.php
+++ app/models/ApiAccessManager.php
@@ -12,12 +12,11 @@ class ApiAccessManager extends MainModel
         parent::__construct();
     }
 
-    public function addAccess($userId, $phoneNumber, $key)
+    public function addAccess($userId, $phoneNumber)
     {
         $apiAccess = new self();
         $apiAccess->userId = $userId;
         $apiAccess->phoneNumber = $phoneNumber;
-        $apiAccess->apiKey = $key;
         $apiAccess->save();
     }
 
@@ -29,8 +28,46 @@ class ApiAccessManager extends MainModel
 
     public function getAccess($phoneNumber, $key)
     {
-        $apiAccess = $this->load(["phoneNumber = ? AND apiKey = ?", $phoneNumber, $key]);
+        $key = explode('*', $key);
+        $apiAccess = $this->load(["phoneNumber = ? AND apiKey = ? AND token = ?", $phoneNumber, $key[0], $key[1]]);
         $apiAccess = $apiAccess->query[0];
         return $apiAccess->userid;
+    }
+
+    public function phoneExists($phoneNumber)
+    {
+        return ($this->count(['phoneNumber = ?', $phoneNumber]) == 1);
+    }
+
+    public function addPhone($phoneNumber, $datas)
+    {
+        $this->load(['phoneNumber = ?', $phoneNumber]);
+        $this->apiKey = $this->generateKey($datas);
+        $this->token = $this->generateToken();
+        $this->model = $datas['model'];
+        $this->platform = $datas['platform'];
+        if($this->save())
+            return ['ApiKey' => $this->apiKey, 'Token' => $this->token];
+        return false;
+    }
+
+    /**
+     * @param $datas
+     * @return string
+     */
+    public function generateKey($datas)
+    {
+        $key = implode($datas);
+        $key = md5($key);
+        $key = str_shuffle($key);
+        $key = substr($key, 0, 10);
+        return $key;
+    }
+
+    public function generateToken()
+    {
+        $token = str_shuffle(rand(0,5000).time());
+        return 'GSB-'.substr($token, 0, 5);
+
     }
 }
