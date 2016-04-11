339691bcb7ab5dd44b501693273ccc55b7b60122
Anicet REGLAT VIZZAVONA <anicet.reglat@gmail.com>
2016-04-11 12:10:18 +0200

    Corrections SMS

--- app/helpers/SMS.php
+++ app/helpers/SMS.php
@@ -9,22 +9,36 @@
 namespace App\Helpers;
 
 use Plivo\RestAPI;
+use Base;
 
 class SMS
 {
-    public static function send($phoneNumber, $token){
-        $auth_id = "MAY2E0NTJLMJM4MTE5ZT";
-        $auth_token = "OWQ2ZDgxNzlmZmU4ZDMwYzNhMWQ0ZmE5M2JjMmE1";
+    /**
+     * Permet d'envoyer un SMS
+     *
+     * @param $phoneNumber Le numero de téléphone
+     * @param $token Le message
+     */
+    public static function send($phoneNumber, $message){
+        $f3 = Base::instance();
+        $auth_id = $f3->get('sms.apiKey');
+        $auth_token = $f3->get('sms.token');
         $p = new RestAPI($auth_id, $auth_token);
         // Send a message
         $params = array(
             'src' => 'GSB',
             'dst' => self::formatPhoneNumber($phoneNumber),
-            'text' => $token
+            'text' => $message
         );
         $p->send_message($params);
     }
 
+    /**
+     * Permet de formater le numero de telephone au format international
+     *
+     * @param $phoneNumber Numero de téléphone
+     * @return string Numéro de téléphone avec le code pays
+     */
     public static function formatPhoneNumber($phoneNumber){
         $phoneNumber = substr($phoneNumber, 1);
         $phoneNumber = '33'.$phoneNumber;
